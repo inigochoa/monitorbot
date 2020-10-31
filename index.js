401850fcf4aa31acba528150136155b5cdbde0c2
Iñigo Ochoa <inigochoa@gmail.com>
2020-10-31 16:13:57 +0100

    Added chat ID middleware filter

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ if (undefined === process.env.TELEGRAM_TOKEN || '' === process.env.TELEGRAM_TOKE
     process.exit(1)
 }
 
+if (undefined === process.env.TELEGRAM_TO || '' === process.env.TELEGRAM_TO) {
+    console.error('You have to set the environment variable TELEGRAM_TO with the ID of the target chat')
+    process.exit(1)
+}
+
 const { Telegraf } = require('telegraf')
 
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
+
+bot.use(async ({ chat }, next) => {
+    if (parseInt(process.env.TELEGRAM_TO) !== chat.id) {
+        return
+    }
+
+    await next()
+})
 
 bot.launch()
 
