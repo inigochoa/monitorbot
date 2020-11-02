7aadca7b67c717c30b7c1f58cd5fc4d96f09b9d1
Iñigo Ochoa <inigochoa@gmail.com>
2020-11-02 16:01:46 +0100

    Replaced telegraf-i18n with i18n for node

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 require('dotenv').config()
 
+const i18n = require('i18n')
+const path = require('path')
+const emoji = require('node-emoji')
+
+i18n.configure({
+    directory: path.join(__dirname, 'locales'),
+    objectNotation: true,
+    retryInDefaultLocale: true,
+})
+i18n.setLocale(process.env.LOCALE)
+
 if (undefined === process.env.TELEGRAM_TOKEN || '' === process.env.TELEGRAM_TOKEN) {
-    console.error('You have to set the environment variable TELEGRAM_TOKEN with the value from @BotFather')
+    console.error(i18n.__('error.token'))
     process.exit(1)
 }
 
 if (undefined === process.env.TELEGRAM_TO || '' === process.env.TELEGRAM_TO) {
-    console.error('You have to set the environment variable TELEGRAM_TO with the ID of the target chat')
+    console.error(i18n.__('error.target'))
     process.exit(1)
 }
 
 if (undefined === process.env.DATABASE_URL || '' === process.env.DATABASE_URL) {
-    console.error('You have to set the environment variable DATABASE_URL with the URL of a PostgreSQL database')
+    console.error(i18n.__('error.database'))
     process.exit(1)
 }
 
@@ -22,21 +33,8 @@ const pool = new Pool({
 })
 
 const { Telegraf } = require('telegraf')
-const TelegrafI18n = require('telegraf-i18n')
-const path = require('path')
-const emoji = require('node-emoji')
-
-const i18n = new TelegrafI18n({
-    useSession: true,
-    defaultLanguage: process.env.LOCALE || 'en',
-    defaultLanguageOnMissing: true,
-    directory: path.resolve(__dirname, 'locales')
-})
-
 const bot = new Telegraf(process.env.TELEGRAM_TOKEN)
 
-bot.use(Telegraf.session())
-bot.use(i18n.middleware())
 bot.use(async ({ chat }, next) => {
     if (parseInt(process.env.TELEGRAM_TO) !== chat.id) {
         return
@@ -45,16 +43,18 @@ bot.use(async ({ chat }, next) => {
     await next()
 })
 
-bot.start((ctx) => ctx.reply(emoji.emojify(ctx.i18n.t('start'))))
+bot.start(({ from, reply}) => {
+    reply(emoji.emojify(i18n.__('command.start', { name: from.first_name })))
+})
 
 const selectQuery = 'SELECT * FROM websites'
 
 bot.command('list', (ctx) => {
     pool.query(selectQuery)
-    .then((res) => ctx.reply(ctx.i18n.t('list') + '\n\n' + res.rows.map((website) => website.url).join('\n')))
+    .then((res) => ctx.reply(i18n.__('command.list') + '\n\n' + res.rows.map((website) => website.url).join('\n')))
     .catch((err) => console.error(err.stack))
 })
 
 bot.launch()
 
-console.info('Monitor bot launched')
+console.info(i18n.__('launched'))
